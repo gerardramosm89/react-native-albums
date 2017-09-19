b7735c964b623a89d724ab55e7cee4038301b514
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-09-19 16:12:59 -0700

    adding card

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 export default class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: []
+    }
+  }
+  componentWillMount() {
+    console.log("Album List mounted!");
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => {
+        console.log('response from api was: ', res.data);
+        this.setState({
+          albums: res.data
+        });
+      })
+      .catch(err => {
+        console.log('got an error: ', err);
+      });
+  }
+  renderAlbums() {
+    return this.state.albums.map(album => {
+      return(
+          <AlbumDetail key={album.title} album={album} />
+      );
+    });
+  }
   render() {
     return (
       <View>
         <Text>Album List!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
