e6699078c72c5ab6a6e15dcee4efbd2b79d8eb0d
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-09-19 17:28:38 -0700

    adding styles

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,20 +4,24 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, image, artist, thumbnail_image } = album;
+  const { thumbnailContainerStyle, headerTextStyle } = styles;
   return(
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image 
           style={styles.thumbnailStyle}
           source={{uri: `${thumbnail_image}`}}
           />
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>     
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={styles.imageStyle} source={{uri: image}}/>
       </CardSection>
     </Card>
   );
@@ -28,9 +32,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
-    width: 50
+    width: 50,
+    borderRadius: 5
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 export default AlbumDetail;
