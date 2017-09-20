3b31b0439c0fefbff10f58cfa22a8f28f606e68d
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-09-19 17:59:07 -0700

    adding button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, image, artist, thumbnail_image } = album;
@@ -22,6 +23,9 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
       <CardSection>
         <Image style={styles.imageStyle} source={{uri: image}}/>
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
